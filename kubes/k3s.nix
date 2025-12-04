e6844935bd6c35568e297f2b004bce684dc92f45
peturparkur <61064454+peturparkur@users.noreply.github.com>
2025-12-04 21:13:46 +0100

    update flakes and k3s fixes
    * flake update
    
    * increase download buffer for laptop
    
    * set node-ip based on eth0 config
    
    * bracket to actually extract the address

--- kubes/k3s.nix
+++ kubes/k3s.nix
@@ -1,4 +1,27 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  default_flags = [
+    # "--tls-san 192.168.1.32"
+
+    # network interface
+    "--flannel-iface eth0" # this should be eth0
+
+    ### SERVER CONFIGURATION ###
+    "--write-kubeconfig-mode 644" # server config
+    # disable pre-packaged components
+    "--disable traefik"
+    "--disable local-storage"
+    "--disable servicelb"
+
+    # kubelet arguments
+    # to disable ipv6 and allow some odd all source allowed ipv4
+    # "--kubelet-arg=allowed-unsafe-sysctls=net.ipv4.conf.all.src_valid_mark,net.ipv6.conf.all.disable_ipv6"
+    # "--allowed-unsafe-sysctls=net.ipv4.conf.all.src_valid_mark,net.ipv6.conf.all.disable_ipv6"
+  ];
+  eth0_addresses = config.networking.interfaces.eth0.ipv4.addresses;
+  node_ip = (builtins.elemAt eth0_addresses 0).address;
+  k3s_flags = default_flags ++ [ "--node-ip ${node_ip}" ];
+in {
   # we want our k3s to support longhorn
   imports = [ ./longhorn.nix ];
 
@@ -9,24 +32,7 @@
     package = pkgs.k3s_1_33;
     serverAddr = "https://192.168.1.32:6443";
     # configPath = ./config.yaml;
-    extraFlags = toString [
-      # "--tls-san 192.168.1.32"
-
-      # network interface
-      "--flannel-iface eth0" # this should be eth0
-
-      ### SERVER CONFIGURATION ###
-      "--write-kubeconfig-mode 644" # server config
-      # disable pre-packaged components
-      "--disable traefik"
-      "--disable local-storage"
-      "--disable servicelb"
-
-      # kubelet arguments
-      # to disable ipv6 and allow some odd all source allowed ipv4
-      # "--kubelet-arg=allowed-unsafe-sysctls=net.ipv4.conf.all.src_valid_mark,net.ipv6.conf.all.disable_ipv6"
-      # "--allowed-unsafe-sysctls=net.ipv4.conf.all.src_valid_mark,net.ipv6.conf.all.disable_ipv6"
-    ];
+    extraFlags = toString k3s_flags;
     tokenFile = config.sops.secrets.k3s_token.path;
   };
 }
