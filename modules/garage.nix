9121993df0c5632e432c0345d6b5bfaf2ff5e9d1
peturparkur <61064454+peturparkur@users.noreply.github.com>
2026-05-20 21:06:21 +0200

    garage directly running baremetal (#6)
    * garage directly running baremetal
    
    * garage own user + nested secret
    
    * add garage s3 region
    
    * higher compression level and adjust data directory
    
    * more required parameters
    
    * change permissions for garage sops secret
    
    * use 32 bit secret for garage instead of 64 bit
    
    * use garage v2
    
    * public id mapping for garage nodes
    
    * increase block size to 10M
    
    * last changes

--- modules/garage.nix
+++ modules/garage.nix
@@ -1,15 +1,64 @@
-{ config, pkgs, lib, ... }: {
-  sops.secrets."garage/rpc-secret" = { };
+{ config, pkgs, lib, networkTopology, garageNodes, ... }:
+
+let
+  nodeIp = networkTopology.${config.networking.hostName};
+  otherNodeNames = lib.filter (name: name != config.networking.hostName)
+    (lib.attrNames garageNodes);
+  # peers should be taken from garage CLI output of `garage node id`
+  bootstrapPeers =
+    map (name: "${garageNodes.${name}}@${networkTopology.${name}}:3901")
+    otherNodeNames;
+  dataDirPath = "/mnt/data/garage";
+in {
+  sops.secrets."garage/rpc-secret" = {
+    owner = "garage";
+    group = "garage";
+  };
+  sops.secrets."garage/admin-token" = {
+    owner = "garage";
+    group = "garage";
+  };
+
+  users.users.garage = {
+    isSystemUser = true;
+    group = "garage";
+    home = "/var/lib/garage";
+  };
+  users.groups.garage = { };
+
+  environment.etc."garage.toml".group = "garage";
+  environment.etc."garage.toml".mode = "0640";
+  users.users.peter.extraGroups = [ "garage" ];
+
+  systemd.tmpfiles.rules = [ "d ${dataDirPath} 0700 garage garage -" ];
 
   services.garage = {
     enable = true;
+    package = pkgs.garage_2;
     settings = {
       metadata_dir = "/var/lib/garage/meta";
-      data_dir = "/mnt/storage/garage";
+      data_dir = lib.mkDefault [{
+        path = dataDirPath;
+        capacity = "100G";
+      }];
+      replication_factor = 2;
+      compression_level = 12;
+      block_size = "10M";
       rpc_bind_addr = "[::]:3901";
+      rpc_public_addr = "${nodeIp}:3901";
       rpc_secret_file = config.sops.secrets."garage/rpc-secret".path;
-      s3_api.address = "[::]:3900";
+      bootstrap_peers = bootstrapPeers;
+      s3_api.api_bind_addr = "[::]:3900";
+      s3_api.s3_region = "garage";
+      s3_api.root_domain = ".s3.garage";
+      s3_web.bind_addr = "[::]:3902";
+      s3_web.root_domain = ".web.garage";
+      admin.api_bind_addr = "[::]:3903";
+      admin.admin_token_file = config.sops.secrets."garage/admin-token".path;
     };
   };
 
+  systemd.services.garage.serviceConfig.DynamicUser = lib.mkForce false;
+  systemd.services.garage.serviceConfig.User = "garage";
+  systemd.services.garage.serviceConfig.Group = "garage";
 }
