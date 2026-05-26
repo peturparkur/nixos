219b38a103d9d8a7bff90273a953f27a7bfba7b5
peturparkur <peter@nagymathe.xyz>
2026-05-26 20:54:33 +0200

    be explicit about docker dns

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,6 +1,15 @@
 { config, pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [ docker-compose ];
+  environment.systemPackages = with pkgs; [ docker-compose docker-buildx ];
+
+  # Symlink docker-buildx into the CLI plugins directory so Docker Compose
+  # can find it when using the Bake backend. Even though `docker buildx`
+  # works via PATH, Compose v2 looks in the plugin directory specifically.
+  # system.activationScripts.docker-buildx = ''
+  #   mkdir -p /usr/lib/docker/cli-plugins
+  #   ln -sf ${pkgs.docker-buildx}/bin/docker-buildx /usr/lib/docker/cli-plugins/docker-buildx
+  # '';
+
   virtualisation.docker = {
     enable = true;
     # start dockerd on boot.
@@ -10,6 +19,7 @@
     # to allow ipv6 in docker
     daemon.settings = {
       ipv6 = true;
+      dns = [ "1.1.1.1" "8.8.8.8" ];
       fixed-cidr-v6 = "fd00::/80";
     };
   };
